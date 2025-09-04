07ae5015cf82766042fef00f23192e9ffa1d379e
Daniel Mezker <dmezker06@gmail.com>
2025-09-04 11:51:11 +0200

    login Style 1

--- index.js
+++ index.js
@@ -7,16 +7,21 @@ const app = express();
 // Server port
 const PORT = process.env.PORT || 3000;
 
+//Zugriffsordner auf dem Server für statische Dateien
+app.use(express.static('public'));
+
 // ejs als View Engine setzen
 app.set('view engine', 'ejs');
 
 
 // index.ejs rendern
 app.get('/', (req, res) => {
-    res.render('index', { name: 'Alice'});
-})
+    res.render('pages/index');
+});
 
+// Routes
 
+// Server starten
 app.listen(PORT, () => {
     console.log(`Server running on Port ${PORT}`);
-})
+});
